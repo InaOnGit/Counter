3e1570ee1665713713181ddb263fd2309438485e
InaOnGit <ina.levankova.net@gmail.com>
2023-02-21 20:20:16 +0100

    adding of a label and a button outlets adding a counter action

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pressButton: UIButton?
+    @IBOutlet weak var CountLabel: UILabel?
+    private var countNumber: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressToCount(_ sender: Any) {
+        countNumber += 1
+        CountLabel?.text = "Значение счетчика: \n " + "\(countNumber)"
+        
+    }
+    
 }
 
